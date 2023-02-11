54e46dd24358f8bd6387f9c9cfb508daece32507
Nurdaulet Akhmatov <70795559+nurdashymside@users.noreply.github.com>
2023-02-11 22:54:04 +0600

    Add files via upload

---
+++ src/resumeData.js
@@ -0,0 +1,98 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Akhmatov Nurdaulet",
+    "role": "Back end developer",
+    "linkedinId":" https://www.linkedin.com/in/nurdamiron",
+    "roleDescription": "Software and Hardware developed",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nurdamiron",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/nurdashymside",
+          "className":"fa fa-github"
+        },
+
+      ],
+    "aboutme":"I am currently a third year student at The Suleymen Demirel University. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning in Hardware technologies. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Kazakhstan",
+    "website":"https://nurdamiron.github.io",
+    "education":[
+      {
+        "UniversityName":"The LNM Insitute of Information Technology",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Hardware Development"
+      },
+      {
+        "skillname":"Software Development"
+      },
+      {
+        "skillname":"Data analysis"
+      },
+
+    ],
+    "portfolio":[
+      {
+        "name":"Fiasco Price",
+        "description":"Data-analysing web-platform",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"Laundry Services",
+        "description":"Hardware & Software integrated project",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"Fiasco Price bot",
+        "description":"Chat-bot for monitoring prices in Kaspi.kz",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"SmartHome",
+        "description":"Smart eco-system with web-app control",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"If you want to build your own software you must build your own Hardware.",
+        "name":"Steve Jobs"
+      },
+    ]
+  }
+  
+  export default resumeData
